30619a03370aeba6bc785a0d7662dd38fa41bb51
Aashish Raj <rajaashish2005@gmail.com>
2024-06-06 02:21:42 +0530

    dekho dekho

--- app.js
+++ app.js
@@ -2,7 +2,21 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 const logo = () => (
-    
+    <div id="logo">
+        <img src="Images\152276850.jfif" alt="a" />
+    </div>
+);
+
+const searchbar = () => (
+    <div id="searchbar">
+        <input type="text" placeholder="Search..." />
+    </div>
+);
+
+const user =() => (
+    <div id="user">
+        <img src="Images\profile.png" alt="u" />dddddddddddddddddddddddddddddddddddddddddddddddddd
+    </div>
 );
 const Header = () => (
     <div id="header">
